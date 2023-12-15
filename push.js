618ca4da36478f66b179c626e95d3418366cdcf7
rino james <rinojames.krl@gmail.com>
2023-12-15 17:09:25 +0530

    try fix

--- push.js
+++ push.js
@@ -12,6 +12,15 @@ push.setVapidDetails(
   vapIDKeys.privateKey
 );
 
-let sub = {};
+let sub = {
+  endpoint:
+    "https://updates.push.services.mozilla.com/wpush/v2/gAAAAABlfDjgqSrFsOBreR4Qp6S-y8mCvQos1iGr9ZgaSN8CBJiIphIjq1nHtnddHzUVPJs-At_t4DsIdzTh1XAi2-i4Wu1D3JIssROcJ9CfpG2TceJVfX0xtA1R5AGFInrN6gn1AU1awyR13oYRUGiF-1Z7WmyR_sjbvj2e99u6_FWQzst8PZg",
+  expirationTime: null,
+  keys: {
+    auth: "Jq72iraQoPZ0FKs555VoFQ",
+    p256dh:
+      "BBE55n2twz0TnQD1yHqXfz5eJBql71VTUjF7c8wDpYL-TYSslODW0oddjjtVkGU_biCGFCHepkNnlIWtBVAsmAA",
+  },
+};
 
 push.sendNotification(sub, "test message");
